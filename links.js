636576115a4fa2431b6d907a2b851be66132002f
JuanAntu <89953621+JuanAntu@users.noreply.github.com>
2026-09-24 22:07:44 -0300

    Web de preguntas al azar para la obra

---
+++ links.js
@@ -0,0 +1,9 @@
+// Una pregunta por línea. Para cambiar una, reemplazá el link entre comillas.
+// Podés agregar o quitar líneas (cada una termina con coma).
+window.LINKS = [
+  "https://canva.link/f4v3dljfl72v3c3",
+  "https://canva.link/te71c3sfkpl6zg3",
+  "https://canva.link/dngbrh4l7xivtn3",
+  "https://canva.link/lf77niamzh5ebjg",
+  "https://canva.link/2fifbieuimx59t6",
+];
